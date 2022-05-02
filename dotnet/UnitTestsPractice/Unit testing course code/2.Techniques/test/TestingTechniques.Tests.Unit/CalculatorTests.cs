00e83233ca6771de4ff7979e7d83e08482faec60
JDN89 <jan.de.niels@gmail.com>
2022-05-02 13:11:11 -0500

    add dotnet unit tests practice folder

---
+++ dotnet/UnitTestsPractice/Unit testing course code/2.Techniques/test/TestingTechniques.Tests.Unit/CalculatorTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TestingTechniques.Tests.Unit;
+
+public class CalculatorTests
+{
+    private readonly Calculator _sut = new();
+
+    [Theory]
+    [InlineData(5, 5, 10)]
+    [InlineData(-5, 5, 0)]
+    [InlineData(-15, -5, -20)]
+    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(
+        int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Add(a, b);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 0)]
+    [InlineData(15, 5, 10)]
+    [InlineData(-5, -5, 0)]
+    [InlineData(-15, -5, -10)]
+    [InlineData(5, 10, -5)]
+    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers(
+        int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Subtract(a, b);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 25)]
+    [InlineData(50, 0, 0)]
+    [InlineData(-5, 5, -25)]
+    public void Multiply_ShouldMultiplyTwoNumbers_WhenTwoNumbersAreIntegers(
+        int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Multiply(a, b);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 1)]
+    [InlineData(15, 5, 3)]
+    public void Divide_ShouldDivideTwoNumbers_WhenTwoNumbersAreIntegers(
+        int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Divide(a, b);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ dotnet/UnitTestsPractice/Unit testing course code/LearningMaterial/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace CalculatorLibrary.Tests.Unit;
+
+public class CalculatorTests
+{
+    private readonly Calculator _sut = new();
+
+    [Theory]
+    [InlineData(5, 4, 9)]
+    [InlineData(0, 0, 0)]
+    [InlineData(-5, -5, -10)]
+    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(
+        int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Add(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers()
+    {
+        // Act
+        var result = _sut.Subtract(10, 5);
+
+        // Assert
+        Assert.Equal(5, result);
+    }
+}
