ef6320de3a393f5b4245b1e7c4b2708343cd90b2
JDN89 <jan.de.niels@gmail.com>
2022-05-16 09:30:45 -0500

    add a small frust app: hello fellow Rustaceans

---
+++ rust/hello-rust/target/debug/deps/hello_rust-f3b4820bf32b8c7f.d
@@ -0,0 +1,5 @@
+/home/jan/Coding/my-playground/rust/hello-rust/target/debug/deps/hello_rust-f3b4820bf32b8c7f.rmeta: src/main.rs
+
+/home/jan/Coding/my-playground/rust/hello-rust/target/debug/deps/hello_rust-f3b4820bf32b8c7f.d: src/main.rs
+
+src/main.rs:
