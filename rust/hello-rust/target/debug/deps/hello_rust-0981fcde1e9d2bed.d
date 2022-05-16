e2be3331749643ee61ed7ba17a25daf5517281bd
JDN89 <jan.de.niels@gmail.com>
2022-05-16 09:16:46 -0500

    hello rust

---
+++ rust/hello-rust/target/debug/deps/hello_rust-0981fcde1e9d2bed.d
@@ -0,0 +1,5 @@
+/home/jan/Coding/my-playground/rust/hello-rust/target/debug/deps/hello_rust-0981fcde1e9d2bed: src/main.rs
+
+/home/jan/Coding/my-playground/rust/hello-rust/target/debug/deps/hello_rust-0981fcde1e9d2bed.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/jan/Coding/my-playground/rust/hello-rust/target/debug/hello-rust: /home/jan/Coding/my-playground/rust/hello-rust/src/main.rs
