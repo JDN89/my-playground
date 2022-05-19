c7aa72663ca9992c657feb438e429b1608774101
JDN89 <jan.de.niels@gmail.com>
2022-05-19 12:35:19 -0500

    rust:loops

--- rust/the_rust_programming_language/control_flow/src/main.rs
+++ rust/the_rust_programming_language/control_flow/src/main.rs
@@ -9,4 +9,37 @@ fn main() {
     } else {
         println!("number is not divisible by 2 or 3 or 4")
     }
+
+    // loops
+    // break and continue apply on inner loop
+    // label outer loops to use the break and continue keywords
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {}", count);
+
+    // return a value from a loop
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 3; // place the value you wanto to return after the break keyword;
+        }
+    };
+    println!("the value of result is {}", result);
 }
