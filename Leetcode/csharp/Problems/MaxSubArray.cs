8f41299cc685f588c65dbbd63c094537f3aca2fe
JDN89 <jan.de.niels@gmail.com>
2022-05-21 17:39:56 -0500

    Kandan's algorytm, max subArray

--- Leetcode/csharp/Problems/MaxSubArray.cs
+++ Leetcode/csharp/Problems/MaxSubArray.cs
@@ -15,8 +15,20 @@ namespace csharp.Problems
     public class MaxSubArray
     {
        public static int FindMax (int[] nums) {
+           var current_sum = nums[0];
+           var max_sum = nums[0];
+            for(int i = 1; i < nums.Length; i++) {
+                int num = nums[i];
+            //compare curent sum value with value of current sum+1
+            current_sum = Math.Max(nums[i], current_sum+nums[i]);
+            //Console.WriteLine(num);
+            Console.WriteLine(current_sum);
+                       // Console.WriteLine(max_sum);
 
-           return nums[0];
+            max_sum = Math.Max(current_sum,max_sum);
+            }
+
+           return max_sum;
 
         }
     }
