8e63e9a3a2d513797533d36b2b27fd3d2bc29d31
JDN89 <jan.de.niels@gmail.com>
2022-05-26 12:47:51 -0500

    study: match control flowi - part2

--- rust/the_rust_programming_language/enums-and-pattern-matching/the-match-control-flow/src/main.rs
+++ rust/the_rust_programming_language/enums-and-pattern-matching/the-match-control-flow/src/main.rs
@@ -2,6 +2,9 @@
 // variant(value)
 //}
 
+//Matches in Rust are exhaustive: we must exhaust every last possibility in order for the code to be valid.
+//n Rust prevents us from forgetting to explicitly handle the None case, it protects us from assuming that we have a value when we might have null, thus making the billion-dollar mistake discussed earlier impossible.
+
 enum Address {
     Zonnestraat,
     Eignestraat,
@@ -17,4 +20,14 @@ fn main() {
     }
     let resutl = wie_woont_waar(Address::Eignestraat);
     println!("{}", resutl);
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
