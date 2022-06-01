efdfde66fa8376022f574ecb4dab0261aa638dee
JDN89 <jan.de.niels@gmail.com>
2022-06-01 16:29:09 -0500

    vectors in rust

---
+++ rust/the_rust_programming_language/collections/vectors/src/lib.rs
@@ -0,0 +1,18 @@
+ let v = vec![1,2,3];
+
+
+
+//let mut v = Vec::new();
+// v.push(5);
+// v.push(6);
+
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
