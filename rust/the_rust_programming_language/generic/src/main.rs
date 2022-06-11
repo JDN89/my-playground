e11a116ee6c04a9c0d47ac1838f4db9b6a4283a8
JDN89 <jan.de.niels@gmail.com>
2022-06-11 12:16:40 -0500

    generics in rust

--- rust/the_rust_programming_language/generic/src/main.rs
+++ rust/the_rust_programming_language/generic/src/main.rs
@@ -1,5 +1,7 @@
+// using  std::cmp::PartialOrd
 //create function that takes a list
-fn largest(list: &[i32]) -> i32 {
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
@@ -10,9 +12,21 @@ fn largest(list: &[i32]) -> i32 {
     // we return largest -> without ; at the end
     largest
 }
+// example of GENERICS in sstructs
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
 
 fn main() {
     let number_list = vec![2, 5, 11, 6, 44, 33];
     let result = largest(&number_list);
+    //CAll genereic STRUCT
+    let mix = Point { x: 4, y: 4.5 };
+
     println!("{}", result);
+    println!("{:?}", mix);
+    println!("{:?}", mix.x);
+    println!("{:?}", mix.y);
 }
