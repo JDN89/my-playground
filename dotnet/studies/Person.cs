a0b58cb7da1d49af487e9f3c1ae538f67a27bbcc
JDN89 <jan.de.niels@gmail.com>
2022-06-17 20:41:48 -0500

    review abstract and public classes plus ref vs valu types

---
+++ dotnet/studies/Person.cs
@@ -0,0 +1,33 @@
+
+
+class Person : Weirdo
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+
+
+
+}
+
+abstract class Weirdo
+{
+    public void comment()
+    {
+        Console.WriteLine("run boy run");
+    }
+    public static void staticComment()
+    {
+        Console.WriteLine("I comment without being instantiated!");
+    }
+}
+
+abstract class Pussy
+{
+    public void MorethenOne()
+    {
+        Console.WriteLine("Can I instantiate more then one class?"); // niet mogelijk
+    }
+
+}
+
---
+++ dotnet/studies/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Person jan = new Person
+{
+    Name = "Juan",
+    Age = 25
+};
+
+
+
+Console.WriteLine(jan.Age);
+
+Person dork = jan;
+// jan is using the public method from the abstract class
+// public method => need to instantiate class and call method on instantiated class
+// belongs to an instance of a class <> static belongs to an class
+jan.comment();
+
+//static method => you can only call this on the Class without instantiating it
+
+Person.staticComment();
+
+dork.Age = 30;
+Console.WriteLine(jan.Age); // output is 30
+
+/* you can instantiate an abstract clas
+Weirdo bert = new Weirdo
+{
+
+    Name = "Juan",
+    Age = 25
+}
+
+*/
