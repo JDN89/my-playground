27fcd8d57b54ca55b7c82bb8447580794ce6f25c
JDN89 <jan.de.niels@gmail.com>
2022-07-05 16:16:44 -0500

    update: add functionality

---
+++ dotnet/trainingFullStack/webUI/my-vitesse-app/src/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// const name = $ref('')
+
+const router = useRouter()
+const go = () => {
+  router.push('/test')
+}
+</script>
+
+<template>
+  <div>
+    <button class="border-light-50 border-2 p-2" @click="go">
+      click Me
+    </button>
+  </div>
+</template>
