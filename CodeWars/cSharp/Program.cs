9a7b2ae3a26a38792168409e5fcb63a8cfa06057
JDN89 <jan.de.niels@gmail.com>
2022-07-06 12:12:50 -0500

    learning: codewars filter el from arr

--- CodeWars/cSharp/Program.cs
+++ CodeWars/cSharp/Program.cs
@@ -1,7 +1,10 @@
-﻿UniqueNumber.GetUnique(new int[] { 1, 1, 1, 2, 1, 1 });
+﻿using cSharp.codewars;
 
-UniqueNumber.GetUnique(new int[] { 1, 2, 2, 2 });
+// UniqueNumber.GetUnique(new int[] { 1, 1, 1, 2, 1, 1 });
 
+// UniqueNumber.GetUnique(new int[] { 1, 2, 2, 2 });
+
+CategorizeNewMembers.OpenOrSenior((new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } }));
 // See https://aka.ms/new-console-template for more information
 /*sortTheOdd.SortArray(new int[] { 7, 1 });
 sortTheOdd.SortArray(new int[] { 5, 8, 6, 3, 4 });
