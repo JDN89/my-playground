ce69458c66973af0779cb2644648eb23efb0e481
JDN89 <jan.de.niels@gmail.com>
2022-07-05 19:30:26 -0500

    learning: connect frontend to bakcend and try out fetch api

--- dotnet/trainingFullStack/api/Program.cs
+++ dotnet/trainingFullStack/api/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -17,6 +18,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseAuthorization();
 
--- dotnet/trainingFullStack/webUI/my-vitesse-app/src/pages/index.vue
+++ dotnet/trainingFullStack/webUI/my-vitesse-app/src/pages/index.vue
@@ -2,15 +2,48 @@
 // const name = $ref('')
 
 const router = useRouter()
+let message = $ref('')
+const postMessage = $ref('')
+let deleteMessage = $ref('')
 const go = () => {
   router.push('/test')
+}
+
+const getData = () => {
+  fetch('https://localhost:7096/api/test').then(res => res.text()).then(t => message = t)
+}
+const deleteData = () => {
+  fetch('https://localhost:7096/api/test', { method: 'Delete' }).then(res => res.text()).then(t => deleteMessage = t).catch(Error => console.error(Error))
+}
+const postData = (postMessage: string) => {
+  fetch('https://localhost:7096/api/test', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title: postMessage }),
+  }).then(res => console.log(res.status))
 }
 </script>
 
 <template>
-  <div>
-    <button class="border-light-50 border-2 p-2" @click="go">
+  <div class="flex mx-auto flex-col flex-wrap ">
+    <button class=" border-light-50 border-2 p-2 flex-wrap" @click="go">
       click Me
     </button>
+    <button @click="getData">
+      Fetch data from api
+    </button>
+    <button @click="deleteData">
+      Delete me
+    </button>
+    <h2 v-if="message" class="border-1 border-light-50">
+      {{ message }}
+    </h2>
+    <h2 v-if="deleteMessage" class="border-1 border-light-50">
+      {{ deleteMessage }}
+    </h2>
+    <input v-model="postMessage" type="text" @keyup.enter="postData(postMessage)">
+    <h2>
+      {{ postMessage }}
+    </h2>
   </div>
 </template>
