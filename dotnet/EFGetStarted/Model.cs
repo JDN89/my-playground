79d661cf9f53190d7e1d0c32551f444a1369d6da
JDN89 <jan.de.niels@gmail.com>
2022-07-07 21:36:11 -0500

    Feat: make it nullable

--- dotnet/EFGetStarted/Model.cs
+++ dotnet/EFGetStarted/Model.cs
@@ -32,7 +32,7 @@ public class BloggingContext : DbContext
 public class Blog
 {
     public int BlogId { get; set; }
-    public string Url { get; set; }
+    public string? Url { get; set; }
 
     public List<Post> Posts { get; } = new();
 }
@@ -40,8 +40,8 @@ public class Blog
 public class Post
 {
     public int PostId { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
 
     public int BlogId { get; set; }
     public Blog Blog { get; set; }
