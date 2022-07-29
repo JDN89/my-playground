203dea34a74a8ee80c48dc3b65410689258933a2
Jan <jan.de.niels@gmail.com>
2022-07-29 11:32:41 +0200

    JWT with spring boot

--- java/amigoscode/spring-boot-security-jwt/userservice/src/main/java/com/example/userservice/UserserviceApplication.java
+++ java/amigoscode/spring-boot-security-jwt/userservice/src/main/java/com/example/userservice/UserserviceApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class UserserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
 }
---
+++ java/amigoscode/spring-data-jpa/spring-data-jpa-course-section-7/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,87 @@
+package com.example.demo;
+
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository studentRepository,
+            StudentIdCardRepository studentIdCardRepository) {
+        return args -> {
+            Faker faker = new Faker();
+
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    email,
+                    faker.number().numberBetween(17, 55));
+
+            student.addBook(
+                    new Book("Clean Code", LocalDateTime.now().minusDays(4)));
+
+            student.addBook(
+                    new Book("Think and Grow Rich", LocalDateTime.now()));
+
+            student.addBook(
+                    new Book("Spring Data JPA", LocalDateTime.now().minusYears(1)));
+
+            StudentIdCard studentIdCard = new StudentIdCard(
+                    "123456789",
+                    student);
+
+            student.setStudentIdCard(studentIdCard);
+
+            student.addEnrolment(new Enrolment(
+                    new EnrolmentId(1L, 1L),
+                    student,
+                    new Course("Computer Science", "IT"),
+                    LocalDateTime.now()
+            ));
+
+            student.addEnrolment(new Enrolment(
+                    new EnrolmentId(1L, 2L),
+                    student,
+                    new Course("Amigoscode Spring Data JPA", "IT"),
+                    LocalDateTime.now().minusDays(18)
+            ));
+
+            student.addEnrolment(new Enrolment(
+                    new EnrolmentId(1L, 2L),
+                    student,
+                    new Course("Amigoscode Spring Data JPA", "IT"),
+                    LocalDateTime.now().minusDays(18)
+            ));
+
+
+
+            studentRepository.save(student);
+
+            studentRepository.findById(1L)
+                    .ifPresent(s -> {
+                        System.out.println("fetch book lazy...");
+                        List<Book> books = student.getBooks();
+                        books.forEach(book -> {
+                            System.out.println(
+                                    s.getFirstName() + " borrowed " + book.getBookName());
+                        });
+                    });
+
+        };
+    }
+}
