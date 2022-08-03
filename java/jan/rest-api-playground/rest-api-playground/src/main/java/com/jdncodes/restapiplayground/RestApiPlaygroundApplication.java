3a7083e96c512dafda55c6b518e54b0bd19959af
Jan <jan.de.niels@gmail.com>
2022-08-03 17:10:06 +0200

    init - rest api playground

---
+++ java/jan/rest-api-playground/rest-api-playground/src/main/java/com/jdncodes/restapiplayground/RestApiPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.jdncodes.restapiplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPlaygroundApplication.class, args);
+	}
+
+}
---
+++ java/jan/rest-api-playground/rest-api-playground/src/test/java/com/jdncodes/restapiplayground/RestApiPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdncodes.restapiplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
