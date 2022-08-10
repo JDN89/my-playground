63d2bd797778c1d4d9b780de9112dcd612ebdb78
Jan <jan.de.niels@gmail.com>
2022-08-10 12:34:24 +0200

    add dummy data via faker

--- java/jan/rest-api-playground/restdemo/src/main/java/com/jdn/restdemo/RestdemoApplication.java
+++ java/jan/rest-api-playground/restdemo/src/main/java/com/jdn/restdemo/RestdemoApplication.java
@@ -1,9 +1,11 @@
 package com.jdn.restdemo;
 
+import com.github.javafaker.Faker;
 import com.jdn.restdemo.entity.SecretConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SecretConfigProperties.class)
@@ -11,6 +13,11 @@ public class RestdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestdemoApplication.class, args);
+	}
+
+	@Bean
+	public Faker faker() {
+		return new Faker();
 	}
 
 }
--- java/jan/rest-api-playground/restdemo/src/main/java/com/jdn/restdemo/data/SampleDataLoader.java
+++ java/jan/rest-api-playground/restdemo/src/main/java/com/jdn/restdemo/data/SampleDataLoader.java
@@ -1,5 +1,6 @@
 package com.jdn.restdemo.data;
 
+import com.github.javafaker.Faker;
 import com.jdn.restdemo.model.Address;
 import com.jdn.restdemo.model.Person;
 import com.jdn.restdemo.repository.PersonRepository;
@@ -7,17 +8,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 @Component
 @RequiredArgsConstructor
 public class SampleDataLoader implements CommandLineRunner {
     private final PersonRepository repository;
+    private final Faker faker;
 
 
 
     @Override
     public void run(String... args) throws Exception {
-        Person person = new Person("Jan", "De Niels", "jdn@gmail.com", new Address("Street", "Citry"));
-        repository.save(person);
+//        Person person = new Person("Jan", "De Niels", "jdn@gmail.com", new Address("Street", "Citry"));
+//        repository.save(person);
+        List<Person> people = IntStream.rangeClosed(1,100)
+                .mapToObj(i -> new Person(
+                        faker.name().firstName(),
+                        faker.name().lastName(),
+                        faker.internet().emailAddress(),
+                        new Address(
+                                faker.address().streetAddress(),
+                                faker.address().city()
+                        )
+                )).toList();
+
+        repository.saveAll(people);
     }
 //    private final Faker faker;
 }
