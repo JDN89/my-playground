ca0e9f8d5446d4299d22f5043ba580e860cf0621
Jan <jan.de.niels@gmail.com>
2022-08-10 15:29:38 +0200

    add personController

--- java/jan/rest-api-playground/restdemo/src/main/java/com/jdn/restdemo/controller/PersonController.java
+++ java/jan/rest-api-playground/restdemo/src/main/java/com/jdn/restdemo/controller/PersonController.java
@@ -1,5 +1,27 @@
 package com.jdn.restdemo.controller;
 
+import com.jdn.restdemo.model.Person;
+import com.jdn.restdemo.services.PersonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class PersonController {
+    private final PersonService service;
+
+    @GetMapping("/findall")
+    public Iterable<Person> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping
+    public Page<Person> findAll(@RequestParam int page, @RequestParam int size, @RequestParam String sort) {
+        return service.findAllOnPage(page,size,sort);
+    }
 }
