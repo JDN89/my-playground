28c8dabc3558bf18e4e9ab0a6bfe8d785a8622f3
Jan <jan.de.niels@gmail.com>
2022-08-12 11:03:50 +0200

    Add FindPersonByEmail to Personrepo

--- java/jan/rest-api-playground/restdemo/src/main/java/com/jdn/restdemo/repository/PersonRepository.java
+++ java/jan/rest-api-playground/restdemo/src/main/java/com/jdn/restdemo/repository/PersonRepository.java
@@ -2,7 +2,14 @@ package com.jdn.restdemo.repository;
 
 import com.jdn.restdemo.model.Person;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PersonRepository extends PagingAndSortingRepository<Person,Integer> {
+import java.util.Optional;
+
+@Repository
+public interface PersonRepository extends PagingAndSortingRepository<Person, Integer> {
+    //JBQL
+    //@Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Person> findPersonByEmail(String emailj);
 
 }
