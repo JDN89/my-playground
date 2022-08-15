e3de8d6c57a8c3074aeb2ac6d9085e6a989b2177
Jan <jan.de.niels@gmail.com>
2022-08-15 19:42:42 +0200

    java lang playground

---
+++ java/jan/java-lang-playground/HelloWorld.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class HelloWorld {
+  
+public static void main(String[] args) {
+  System.out.println("hello you");
+
+ var arr = new ArrayList<Integer>();
+    arr.add(2);
+arr.add(3);
+    arr.add(6);
+
+    for (int x : arr) {
+      System.out.println(x);
+      
+    }
+}
+}
+
---
+++ java/jan/java-lang-playground/array/Arraytjes.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Arraytjes {
+ ArrayList<Integer> array = new ArrayList<Integer>();
+
+  
+}
+
+
+
+
