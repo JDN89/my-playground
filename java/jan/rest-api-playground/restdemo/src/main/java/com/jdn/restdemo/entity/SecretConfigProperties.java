eb9e7f11fecac467a62caf09772148251590698a
JDN <jan.de.niels@gmail.com>
2022-10-11 21:54:14 +0200

    cleanup

--- java/jan/rest-api-playground/restdemo/src/main/java/com/jdn/restdemo/entity/SecretConfigProperties.java
+++
@@ -1,8 +0,0 @@
-package com.jdn.restdemo.entity;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-
-@ConfigurationProperties("secret")
-public record SecretConfigProperties(String apiUrl, String apiVersion, String password) {
-}
--- java/jan/rest-api-playground/restdemo/src/main/java/com/jdn/restdemo/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jdn.restdemo.repository;
-
-import com.jdn.restdemo.model.Address;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends CrudRepository<Address, Integer> {
-}
