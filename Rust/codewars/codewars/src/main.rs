e1ac0cd808765bc54ef804796c2540f7379c43e6
JDN <jan.de.niels@gmail.com>
2022-11-07 21:45:53 +0100

    7kyu/string ends with?

--- Rust/codewars/codewars/src/main.rs
+++ Rust/codewars/codewars/src/main.rs
@@ -1,32 +1,30 @@
-pub(crate) fn basic_op(operator: char, value1: i32, value2: i32) -> i32 {
-    let expected: i32 = match operator {
-        '+' => value1 + value2,
-        '-' => value1 - value2,
-        '/' => value1 / value2,
-        '*' => value1 * value2,
-        _ => unreachable!(),
-    };
-    expected
+fn solution(word: &str, ending: &str) -> bool {
+    if ending.len() > word.len() { return false} 
+
+    let len = word.len() - ending.len();
+    let slice = &word[len..];
+    println!("{}",slice);
+    if slice == ending {
+        return true;
+    }
+    false
 }
 
 fn main() {
-    let result = basic_op('-', 4, 5);
+    let result = solution("abd", "abd");
     println!("{}", result)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::basic_op;
-
-    fn dotest(op: char, v1: i32, v2: i32, expected: i32) {
-        let actual = basic_op(op, v1, v2);
-        assert!(actual == expected,
-            "With operator = '{op}', value1 = {v1}, value2 = {v2}\nExpected {expected} but got {actual}")
-    }
+    use super::*;
 
     #[test]
-    fn example_tests() {
-        dotest('+', 4, 7, 11);
-        dotest('-', 5, 4, 1)
+    fn returns_expected() {
+        assert_eq!(true, solution("abc", "abc"));
+    }
+    #[test]
+    fn returns_false() {
+        assert_eq!(false, solution("strawberry", "banana"));
     }
 }
