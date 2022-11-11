e0f57380575c1a20add39ff8a5f49e0467ce8f16
JDN89 <jan.de.niels@gmail.com>
2022-11-11 19:11:13 +0100

    6Kyu: Replace With Alphabet Position part 1

--- Rust/codewars/codewars/src/main.rs
+++ Rust/codewars/codewars/src/main.rs
@@ -1,30 +1,28 @@
-fn solution(word: &str, ending: &str) -> bool {
-    if ending.len() > word.len() { return false} 
 
-    let len = word.len() - ending.len();
-    let slice = &word[len..];
-    println!("{}",slice);
-    if slice == ending {
-        return true;
-    }
-    false
+fn alphabet_position(text: &str) -> String {
+    
+    "done".to_string()
+    // Code here...
 }
 
 fn main() {
-    let result = solution("abd", "abd");
+    let result = alphabet_position("abd");
     println!("{}", result)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+fn returns_expected() {
+    assert_eq!(
+        alphabet_position("The sunset sets at twelve o' clock."),
+        "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11".to_string()
+    );
+    assert_eq!(
+        alphabet_position("The narwhal bacons at midnight."),
+        "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20".to_string()
+    );
+}
 
-    #[test]
-    fn returns_expected() {
-        assert_eq!(true, solution("abc", "abc"));
-    }
-    #[test]
-    fn returns_false() {
-        assert_eq!(false, solution("strawberry", "banana"));
-    }
 }
