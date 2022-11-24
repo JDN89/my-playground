14d9c5539488f203a349fcd48d187b5d02a4d192
Jan <jan.de.niels@gmail.com>
2022-11-24 22:28:19 +0100

    failed codewars attempst - feeling the curve

--- Rust/codewars/codewars/src/main.rs
+++ Rust/codewars/codewars/src/main.rs
@@ -1,40 +1,65 @@
 
-fn alphabet_position(text: &str) -> String {
-    
-    //https://en.wikipedia.org/wiki/ASCII#Printable_characters
-    text
-        .to_lowercase()
-        .chars()
-        .filter( |c|  c >= &'a' && c <= &'z')
-        .map(|c| (c as u32 -96).to_string())
-        .collect::<Vec<String>>()
-        .join(" ")
-    // Code here...
-    //
+// fn alphabet_position(text: &str) -> String {
+//     
+//     //https://en.wikipedia.org/wiki/ASCII#Printable_characters
+//     text
+//         .to_lowercase()
+//         .chars()
+//         .filter( |c|  c >= &'a' && c <= &'z')
+//         .map(|c| (c as u32 -96).to_string())
+//         .collect::<Vec<String>>()
+//         .join(" ")
+//     // Code here...
+//     
+// }
+
+
+// fn spin_words(words: &str) -> String {
+
+// let result = words.split_ascii_whitespace().map(|word| match word.len() > 4 {
+//         true => word.chars().rev().collect(),
+//         false => word.to_string()
+
+//      }  ).collect::<Vec<String>>().join(" ");
+
+//     println!("{:?}",result);
+//     
+//         
+//     return "Hello".to_string()
+
+
+// }
+use itertools::Itertools;
+
+fn remove_smallest(numbers: &[u32]) -> Vec<u32> {
+    let mut numbers = numbers.to_vec();
+    match numbers.iter().position_min() {
+        None => numbers,
+        Some(m) => {numbers.remove(m); numbers}
+    }
 }
 
+
+
+
 fn main() {
-    
-    let x: char = 'a';
-       let b: u32 = x as u32;
-    println!("{}", b);
-    let result = alphabet_position("abd");
-    println!("{}", result)
+     remove_smallest(&[1, 2, 3, 4, 5]);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+        const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
+    
+    fn dotest(a: &[u32], expected: &[u32]) {
+        assert_eq!(remove_smallest(a), expected, "{ERR_MSG} with numbers = {a:?}")
+    }
     #[test]
-fn returns_expected() {
-    assert_eq!(
-        alphabet_position("The sunset sets at twelve o' clock."),
-        "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11".to_string()
-    );
-    assert_eq!(
-        alphabet_position("The narwhal bacons at midnight."),
-        "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20".to_string()
-    );
-}
+    fn fixed_tests() {
+        dotest(&[1, 2, 3, 4, 5], &[2, 3, 4, 5]);
+        dotest(&[1, 2, 3, 4], &[2, 3, 4]);
+        dotest(&[5, 3, 2, 1, 4], &[5, 3, 2, 4]);
+        dotest(&[1, 2, 3, 1, 1], &[2, 3, 1, 1]);
+    }
 
 }
