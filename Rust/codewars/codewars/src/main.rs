5acbcf5fd1a9326e19e7ff56bb46f3d613464b95
JDN89 <jan.de.niels@gmail.com>
2023-01-14 10:40:30 +0100

    7 kyu

--- Rust/codewars/codewars/src/main.rs
+++ Rust/codewars/codewars/src/main.rs
@@ -1,32 +1,39 @@
-fn min_max(lst: &[i32]) -> (i32, i32) {
-    (*lst.iter().min().unwrap(), *lst.iter().max().unwrap())
+fn get_sum(a: i64, b: i64) -> i64 {
+    let mut sum = 0;
+    if a == b {
+        return a;
+    } else if a < b {
+        for i in a..(b + 1) {
+            println!("{:?}", i);
+            sum += i
+        }
+        println!("sum: {:?}", sum);
+        sum
+    } else {
+        for i in b..(a + 1) {
+            sum += i
+        }
+        sum
+    }
 }
 
 fn main() {
-    min_max(&[1, 2, 3, 4, 5]);
+    get_sum(1, 2);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::min_max;
-
-    const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
-
-    fn dotest(arr: &[i32], expected: (i32, i32)) {
-        assert_eq!(min_max(arr), expected, "{ERR_MSG} with lst = {arr:?}")
-    }
+    use super::get_sum;
 
     #[test]
-    fn fixed_tests() {
-        for (arr, expected) in [
-            (vec![1, 2, 3, 4, 5], (1, 5)),
-            (vec![2334454, 5], (5, 2334454)),
-        ] {
-            dotest(&arr, expected)
-        }
+    fn sample_tests() {
+        assert_eq!(get_sum(0, 1), 1);
+        assert_eq!(get_sum(1, 2), 3);
+        assert_eq!(get_sum(5, -1), 14);
+        assert_eq!(get_sum(505, 4), 127759);
     }
 }
-//
+
 //     //https://en.wikipedia.org/wiki/ASCII#Printable_characters
 //     text
 //         .to_lowercase()
