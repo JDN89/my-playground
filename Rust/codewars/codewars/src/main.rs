3dbf8f92d53a81fcfa426703ae95a5dc9931e534
JDN89 <jan.de.niels@gmail.com>
2023-01-15 15:00:19 +0100

    Moves in squared strings

--- Rust/codewars/codewars/src/main.rs
+++ Rust/codewars/codewars/src/main.rs
@@ -1,104 +1,44 @@
-fn get_sum(a: i64, b: i64) -> i64 {
-    let mut sum = 0;
-    if a == b {
-        return a;
-    } else if a < b {
-        for i in a..(b + 1) {
-            println!("{:?}", i);
-            sum += i
-        }
-        println!("sum: {:?}", sum);
-        sum
-    } else {
-        for i in b..(a + 1) {
-            sum += i
-        }
-        sum
-    }
+fn hor_mirror(s: String) -> String {
+    s.split('\n').rev().collect::<Vec<&str>>().join("\n")
 }
-
+fn vert_mirror(s: String) -> String {
+    s.split('\n')
+        .map(|s| s.chars().rev().collect::<String>())
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+fn oper(oper: fn(String) -> String, s: String) -> String {
+    oper(s)
+}
 fn main() {
-    get_sum(1, 2);
+    oper(hor_mirror, "yeCt\nCSbg\nJVhv\nlVHt".to_string());
+
+    oper(vert_mirror, "yeCt\nCSbg\nJVhv\nlVHt".to_string());
 }
 
-#[cfg(test)]
-mod tests {
-    use super::get_sum;
-
-    #[test]
-    fn sample_tests() {
-        assert_eq!(get_sum(0, 1), 1);
-        assert_eq!(get_sum(1, 2), 3);
-        assert_eq!(get_sum(5, -1), 14);
-        assert_eq!(get_sum(505, 4), 127759);
-    }
+fn testing1(s: &str, exp: &str) -> () {
+    assert_eq!(oper(hor_mirror, s.to_string()), exp)
+}
+fn testing2(s: &str, exp: &str) -> () {
+    assert_eq!(oper(vert_mirror, s.to_string()), exp)
 }
 
-//     //https://en.wikipedia.org/wiki/ASCII#Printable_characters
-//     text
-//         .to_lowercase()
-//         .chars()
-//         .filter( |c|  c >= &'a' && c <= &'z')
-//         .map(|c| (c as u32 -96).to_string())
-//         .collect::<Vec<String>>()
-//         .join(" ")
-//     // Code here...
-//
-// }
+#[test]
+fn basics_oper() {
+    testing1("lVHt\nJVhv\nCSbg\nyeCt", "yeCt\nCSbg\nJVhv\nlVHt");
+    testing1("njMK\ndbrZ\nLPKo\ncEYz", "cEYz\nLPKo\ndbrZ\nnjMK");
+    testing1("QMxo\ntmFe\nWLUG\nowoq", "owoq\nWLUG\ntmFe\nQMxo");
+}
 
-// fn spin_words(words: &str) -> String {
-
-// let result = words.split_ascii_whitespace().map(|word| match word.len() > 4 {
-//         true => word.chars().rev().collect(),
-//         false => word.to_string()
-
-//      }  ).collect::<Vec<String>>().join(" ");
-
-//     println!("{:?}",result);
-//f
-//
-//     return "Hello".to_string()
-
-// }
-/* use itertools::Itertools;
-
-fn remove_smallest(numbers: &[u32]) -> Vec<u32> {
-    let mut numbers = numbers.to_vec();
-    match numbers.iter().position_min() {
-        None => numbers,
-        Some(m) => {
-            numbers.remove(m);
-            numbers
-      }
-    }
-} */
-
-// fn largest_five_digit_number(num: &str) -> u32 {
-//     let mut max: u32 = 0;
-//     let len_minus_4 = num.len() - 4;
-//     let total_len = num.len();
-//
-//     println!("{}", len_minus_4);
-//     println!("{}", total_len);
-//
-//     for i in 0..num.len() - 4 {
-//         println!("{}",i);
-//         let value = num.get(i..i + 5).unwrap().parse().unwrap();
-//         if value > max {
-//             max = value
-//         };
-//     }
-//     max
-// }
-//
-// ===========================
-// iterate over Vec and return only non matching values
-
-// fn array_diff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
-//
-//     a.retain(|i| !b.contains(i)).collect();
-// }
-
-// fn array_diff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
-//     a.into_iter().filter(|x| !b.contains(x)).collect()
-// }
+#[test]
+fn basics_oper2() {
+    testing2(
+        "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu",
+        "QHdgSh\noaMDnH\nXxNNlC\nHxxvRi\nAvVTqb\nuRySvw",
+    );
+    testing2(
+        "IzOTWE\nkkbeCM\nWuzZxM\nvDddJw\njiJyHF\nPVHfSx",
+        "EWTOzI\nMCebkk\nMxZzuW\nwJddDv\nFHyJij\nxSfHVP",
+    );
+    testing2("cuQW\nxOuD\nfZwp\neqFx", "WQuc\nDuOx\npwZf\nxFqe");
+}
--- Rust/codewars/codewars/target/debug/codewars.d
+++ Rust/codewars/codewars/target/debug/codewars.d
@@ -1,1 +1,1 @@
-/home/jan/coding/my-playground/Rust/codewars/codewars/target/debug/codewars: /home/jan/coding/my-playground/Rust/codewars/codewars/src/main.rs
+/home/jan/my-playground/Rust/codewars/codewars/target/debug/codewars: /home/jan/my-playground/Rust/codewars/codewars/src/main.rs
