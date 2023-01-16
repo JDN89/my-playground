020346cef992bec6d74a731ceab90e7f6fb9fa6f
JDN89 <jan.de.niels@gmail.com>
2023-01-16 21:12:09 +0100

    7kyu

--- Rust/codewars/codewars/src/main.rs
+++ Rust/codewars/codewars/src/main.rs
@@ -1,44 +1,36 @@
-fn hor_mirror(s: String) -> String {
-    s.split('\n').rev().collect::<Vec<&str>>().join("\n")
+fn get_middle(s: &str) -> &str {
+    let len = s.len();
+    let from = (len - 1) / 2;
+
+    println!("{:?}", from);
+    let to = (len / 2) + 1;
+    println!("{:?}", to);
+    &s[from..to]
 }
-fn vert_mirror(s: String) -> String {
-    s.split('\n')
-        .map(|s| s.chars().rev().collect::<String>())
-        .collect::<Vec<String>>()
-        .join("\n")
-}
-fn oper(oper: fn(String) -> String, s: String) -> String {
-    oper(s)
-}
+
 fn main() {
-    oper(hor_mirror, "yeCt\nCSbg\nJVhv\nlVHt".to_string());
-
-    oper(vert_mirror, "yeCt\nCSbg\nJVhv\nlVHt".to_string());
+    get_middle("test");
+    get_middle("A");
+    get_middle("testi");
 }
-
-fn testing1(s: &str, exp: &str) -> () {
-    assert_eq!(oper(hor_mirror, s.to_string()), exp)
-}
-fn testing2(s: &str, exp: &str) -> () {
-    assert_eq!(oper(vert_mirror, s.to_string()), exp)
-}
-
-#[test]
-fn basics_oper() {
-    testing1("lVHt\nJVhv\nCSbg\nyeCt", "yeCt\nCSbg\nJVhv\nlVHt");
-    testing1("njMK\ndbrZ\nLPKo\ncEYz", "cEYz\nLPKo\ndbrZ\nnjMK");
-    testing1("QMxo\ntmFe\nWLUG\nowoq", "owoq\nWLUG\ntmFe\nQMxo");
-}
+// fn hor_mirror(s: String) -> String {
+//     s.split('\n').rev().collect::<Vec<&str>>().join("\n")
+// }
+// fn vert_mirror(s: String) -> String {
+//     s.split('\n')
+//         .map(|s| s.chars().rev().collect::<String>())
+//         .collect::<Vec<String>>()
+//         .join("\n")
+// }
+// fn oper(oper: fn(String) -> String, s: String) -> String {
+//     oper(s)
+// }
 
 #[test]
-fn basics_oper2() {
-    testing2(
-        "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu",
-        "QHdgSh\noaMDnH\nXxNNlC\nHxxvRi\nAvVTqb\nuRySvw",
-    );
-    testing2(
-        "IzOTWE\nkkbeCM\nWuzZxM\nvDddJw\njiJyHF\nPVHfSx",
-        "EWTOzI\nMCebkk\nMxZzuW\nwJddDv\nFHyJij\nxSfHVP",
-    );
-    testing2("cuQW\nxOuD\nfZwp\neqFx", "WQuc\nDuOx\npwZf\nxFqe");
+fn example_tests() {
+    assert_eq!(get_middle("test"), "es");
+    assert_eq!(get_middle("testing"), "t");
+    assert_eq!(get_middle("middle"), "dd");
+    assert_eq!(get_middle("A"), "A");
+    assert_eq!(get_middle("of"), "of");
 }
