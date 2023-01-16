21d24859d26c84853bb3b000ea4fffa466ea91cc
JDN89 <jan.de.niels@gmail.com>
2023-01-16 22:08:50 +0100

    Highest Scoring Word

--- Rust/codewars/codewars/src/main.rs
+++ Rust/codewars/codewars/src/main.rs
@@ -1,18 +1,24 @@
-fn get_middle(s: &str) -> &str {
-    let len = s.len();
-    let from = (len - 1) / 2;
+fn high(input: &str) -> &str {
+    let v: Vec<&str> = input.split(" ").collect();
+    let mut vec_weight_word = Vec::new();
 
-    println!("{:?}", from);
-    let to = (len / 2) + 1;
-    println!("{:?}", to);
-    &s[from..to]
+    for w in &v {
+        let ascii_values: i32 = w.chars().map(|c| c as i32 - 96).sum();
+        vec_weight_word.push(ascii_values);
+        println!("word: {:?}", ascii_values);
+    }
+
+    let max_value = vec_weight_word.iter().max().unwrap();
+
+    let position_max_word = vec_weight_word.iter().position(|i| i == max_value).unwrap();
+    println!("{:?}", position_max_word);
+    v[position_max_word]
 }
 
 fn main() {
-    get_middle("test");
-    get_middle("A");
-    get_middle("testi");
+    high("man i need a taxi up to ubud");
 }
+
 // fn hor_mirror(s: String) -> String {
 //     s.split('\n').rev().collect::<Vec<&str>>().join("\n")
 // }
@@ -26,11 +32,21 @@ fn main() {
 //     oper(s)
 // }
 
-#[test]
-fn example_tests() {
-    assert_eq!(get_middle("test"), "es");
-    assert_eq!(get_middle("testing"), "t");
-    assert_eq!(get_middle("middle"), "dd");
-    assert_eq!(get_middle("A"), "A");
-    assert_eq!(get_middle("of"), "of");
+#[cfg(test)]
+mod tests {
+    use super::high;
+
+    #[test]
+    fn test_basic() {
+        assert_eq!(high("man i need a taxi up to ubud"), "taxi");
+        assert_eq!(high("what time are we climbing up the volcano"), "volcano");
+        assert_eq!(high("take me to semynak"), "semynak");
+        assert_eq!(high("massage yes massage yes massage"), "massage");
+        assert_eq!(high("take two bintang and a dance please"), "bintang");
+        assert_eq!(high("aa b"), "aa");
+        assert_eq!(high("b aa"), "b");
+        assert_eq!(high("bb d"), "bb");
+        assert_eq!(high("d bb"), "d");
+        assert_eq!(high("aaa b"), "aaa");
+    }
 }
