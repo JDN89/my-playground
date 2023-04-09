9c2015547f179c05a3391102e292c3e7d55435bb
JDN89 <jan.de.niels@gmail.com>
2023-04-09 21:50:01 +0200

    first succesfull parsing of crate

--- nom-nom-nom/src/parser.rs
+++ nom-nom-nom/src/parser.rs
@@ -1,10 +1,43 @@
+use nom::bytes::complete::{tag, take};
+use nom::combinator::map;
+use nom::IResult;
+use nom::sequence::delimited;
 
+#[derive(Debug,PartialEq)]
+pub struct Crate(char);
 
-pub mod parser {
-   pub fn parse ( input:&String) {
-      println!("{input}");
-      
-   }
+pub fn parse_crate(input: &str) -> IResult<&str, Crate> {
+    let first_char_closure = |s: &str| Crate(s.chars().next().unwrap());
+    let crate_parser_function = delimited(tag("["), take(1_usize), tag("]"));
+    map(crate_parser_function, first_char_closure)(input)
 }
 
 
+pub mod parser {
+
+    pub fn parse(input: &String) {
+        // for line in input.lines() {
+        //     let parsed_line = parse_crates(line).expect("parse crate lines");
+        //     println!("{:?}", parsed_line);
+        // }
+        println!("{input}");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse_crate() {
+        let input = "[A]";
+        let result = parse_crate(input);
+        assert_eq!(result, Ok(("", Crate('A'))));
+    }
+
+    // #[test]
+    // fn test_parse_crates() {
+    //     let input = "[a][b][c]";
+    //     let (_, result) = parse_crates(input).unwrap();
+    //     assert_eq!(result, vec!["a", "b", "c"]);
+    // }
+}
